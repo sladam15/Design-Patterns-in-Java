53243c575e95a0fac8c726ea09079940f54ced91
sladam15 <Brandon@Brandon-PC>
2021-04-20 16:35:57 -0700

    Update syntax error

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,7 +18,6 @@ public class Main {
     public static void main(String[] args) {
 
         int cycle = 1;
-        boolean evolve = false;
 
         Trainer trainer = new Trainer(Trainer.Name.ASH);
         trainer.showMessage();  // singleton design pattern
